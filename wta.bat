405e880e448ccb25d7f17d82c1fd213badc6c942
tamaina <tamaina@hotmail.co.jp>
2016-01-12 22:26:49 +0900

    時間計測bat小改良

--- wta.bat
+++ wta.bat
@@ -2,64 +2,64 @@
 
 rem 借りました→http://qiita.com/koryuohproject/items/815a1621bc34a223e4b9
 
-if "%1" == "START"  goto BEGIN
-if "%1" == "STOP"   goto END
-if "%1" == "PRINT"  goto PRINT
+if "%1" == "START"  goto BEGINa
+if "%1" == "STOP"   goto ENDa
+if "%1" == "PRINT"  goto PRINTa
 goto EOF
 
-:BEGIN
-SET T=%TIME: =0%
-SET H=%T:~0,2%
-SET M=%T:~3,2%
-SET S=%T:~6,2%
-SET L=%T:~9,2%
+:BEGINa
+SET Ta=%TIME: =0%
+SET Ha=%Ta:~0,2%
+SET Ma=%Ta:~3,2%
+SET Sa=%Ta:~6,2%
+SET La=%Ta:~9,2%
 
 rem --8進対策
-set /a H=1%H%-100
-set /a M=1%M%-100
-set /a S=1%S%-100
+set /a Ha=1%Ha%-100
+set /a Ma=1%Ma%-100
+set /a Sa=1%Sa%-100
 goto EOF
 
-:END
-SET T1=%TIME: =0%
-SET H1=%T1:~0,2%
-SET M1=%T1:~3,2%
-SET S1=%T1:~6,2%
-SET L1=%T1:~9,2%
+:ENDa
+SET T1a=%TIME: =0%
+SET H1a=%T1a:~0,2%
+SET M1a=%T1a:~3,2%
+SET S1a=%T1a:~6,2%
+SET L1a=%T1a:~9,2%
 
 rem --8進対策
-set /a H1=1%H1%-100
-set /a M1=1%M1%-100
-set /a S1=1%S1%-100
+set /a H1a=1%H1a%-100
+set /a M1a=1%M1a%-100
+set /a S1a=1%S1a%-100
 rem --終了時間の計算
-SET /a H2=H1-H
+SET /a H2a=H1a-Ha
 
-SET /a M2=M1-M
-if %M2% LSS 0 set /a H2=H2-1
-if %M2% LSS 0 set /a M2=M2+60
+SET /a M2a=M1a-Ma
+if %M2a% LSS 0 set /a H2a=H2a-1
+if %M2a% LSS 0 set /a M2a=M2a+60
 
-SET /a S2=S1-S
-if %S2% LSS 0 set /a M2=M2-1
-if %S2% LSS 0 set /a S2=S2+60
+SET /a S2a=S1a-Sa
+if %S2a% LSS 0 set /a M2a=M2a-1
+if %S2a% LSS 0 set /a S2a=S2a+60
 
-SET /a L2=L1-L
-if %L2% LSS 0 set /a S2=S2-1
-if %L2% LSS 0 set /a L2=L2+100
+SET /a L2a=L1a-La
+if %L2a% LSS 0 set /a S2a=S2a-1
+if %L2a% LSS 0 set /a L2a=L2a+100
 
 rem 二けた強制表示
-rem if %L2% LSS 10 set L2=0%L2%
+rem if %L2a% LSS 10 set L2a=0%L2a%
 
-SET /a DPS=%H2%*3600+%M2%*60+%S2%
-SET DPS2=%DPS%.%L2%
+SET /a DPS=%H2a%*3600+%M2a%*60+%S2a%
+SET DPS2=%DPSa%.%L2a%
 
-set DPS_STAMP=%H2%:%M2%:%S2%
-set DPS_STAMP2=%DPS_STAMP%.%L2%
+set DPS_STAMP=%H2a%:%M2a%:%S2a%
+set DPS_STAMP2=%DPS_STAMPa%.%L2a%
 
 goto EOF
 
-:PRINT 
-echo 開始時間：%T%
-echo 終了時間：%T1%
-echo 経過時間：%DPS_STAMP%
-echo 経過秒数：%DPS2%
+:PRINTa
+echo 開始時間：%Ta%
+echo 終了時間：%T1a%
+echo 経過時間：%DPS_STAMPa%
+echo 経過秒数：%DPS2a%
 :EOF
--- wtb.bat
+++ wtb.bat
@@ -2,64 +2,64 @@
 
 rem 借りました→http://qiita.com/koryuohproject/items/815a1621bc34a223e4b9
 
-if "%1" == "START"  goto BEGIN
-if "%1" == "STOP"   goto END
-if "%1" == "PRINT"  goto PRINT
+if "%1" == "START"  goto BEGINb
+if "%1" == "STOP"   goto ENDb
+if "%1" == "PRINT"  goto PRINTb
 goto EOF
 
-:BEGIN
-SET T=%TIME: =0%
-SET H=%T:~0,2%
-SET M=%T:~3,2%
-SET S=%T:~6,2%
-SET L=%T:~9,2%
+:BEGINb
+SET Tb=%TIME: =0%
+SET Hb=%Tb:~0,2%
+SET Mb=%Tb:~3,2%
+SET Sb=%Tb:~6,2%
+SET Lb=%Tb:~9,2%
 
 rem --8進対策
-set /a H=1%H%-100
-set /a M=1%M%-100
-set /a S=1%S%-100
+set /a Hb=1%Hb%-100
+set /a Mb=1%Mb%-100
+set /a Sb=1%Sb%-100
 goto EOF
 
-:END
-SET T1=%TIME: =0%
-SET H1=%T1:~0,2%
-SET M1=%T1:~3,2%
-SET S1=%T1:~6,2%
-SET L1=%T1:~9,2%
+:ENDb
+SET T1b=%TIME: =0%
+SET H1b=%T1b:~0,2%
+SET M1b=%T1b:~3,2%
+SET S1b=%T1b:~6,2%
+SET L1b=%T1b:~9,2%
 
 rem --8進対策
-set /a H1=1%H1%-100
-set /a M1=1%M1%-100
-set /a S1=1%S1%-100
+set /a H1b=1%H1b%-100
+set /a M1b=1%M1b%-100
+set /a S1b=1%S1b%-100
 rem --終了時間の計算
-SET /a H2=H1-H
+SET /a H2b=H1b-Hb
 
-SET /a M2=M1-M
-if %M2% LSS 0 set /a H2=H2-1
-if %M2% LSS 0 set /a M2=M2+60
+SET /a M2b=M1b-Mb
+if %M2b% LSS 0 set /a H2b=H2b-1
+if %M2b% LSS 0 set /a M2b=M2b+60
 
-SET /a S2=S1-S
-if %S2% LSS 0 set /a M2=M2-1
-if %S2% LSS 0 set /a S2=S2+60
+SET /a S2b=S1b-Sb
+if %S2b% LSS 0 set /a M2b=M2b-1
+if %S2b% LSS 0 set /a S2b=S2b+60
 
-SET /a L2=L1-L
-if %L2% LSS 0 set /a S2=S2-1
-if %L2% LSS 0 set /a L2=L2+100
+SET /a L2b=L1b-Lb
+if %L2b% LSS 0 set /a S2b=S2b-1
+if %L2b% LSS 0 set /a L2b=L2b+100
 
 rem 二けた強制表示
-rem if %L2% LSS 10 set L2=0%L2%
+rem if %L2b% LSS 10 set L2b=0%L2b%
 
-SET /a DPS=%H2%*3600+%M2%*60+%S2%
-SET DPS2=%DPS%.%L2%
+SET /a DPS=%H2b%*3600+%M2b%*60+%S2b%
+SET DPS2=%DPSb%.%L2b%
 
-set DPS_STAMP=%H2%:%M2%:%S2%
-set DPS_STAMP2=%DPS_STAMP%.%L2%
+set DPS_STAMP=%H2b%:%M2b%:%S2b%
+set DPS_STAMP2=%DPS_STAMPb%.%L2b%
 
 goto EOF
 
-:PRINT 
-echo 開始時間：%T%
-echo 終了時間：%T1%
-echo 経過時間：%DPS_STAMP%
-echo 経過秒数：%DPS2%
+:PRINTb
+echo 開始時間：%Tb%
+echo 終了時間：%T1b%
+echo 経過時間：%DPS_STAMPb%
+echo 経過秒数：%DPS2b%
 :EOF
